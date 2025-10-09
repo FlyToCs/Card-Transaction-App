2658c9ce5333b1c81998f97b161dd5e16d6159f2
Mohammadhosein Dehghani <dehghani.fullstack@gmail.com>
2025-10-09 19:41:03 +0330

    delete unuseded methods in cardservise

--- Quiz2/Services/CardService.cs
+++ Quiz2/Services/CardService.cs
@@ -35,20 +35,6 @@ public class CardService(ICardRepository cardRepository) : ICardService
         return card;
     }
 
-    public bool CardExist(string cardNumber, string password)
-    {
-        return _cardRepository.CardExist(cardNumber, password);
-    }
-
-    public bool CardIsActive(string cardNumber)
-    {
-       return _cardRepository.CardIsActive(cardNumber);
-    }
-
-    public float GetCardBalance(string cardNumber)
-    {
-        return _cardRepository.GetCardBalance(cardNumber);
-    }
 
     public void UpdateLastTransferDate(string cardNumber, DateTime dateOnly)
     {
@@ -60,30 +46,12 @@ public class CardService(ICardRepository cardRepository) : ICardService
         _cardRepository.UpdateLoginData(cardLoginUpdateDto);
     }
 
-    public void UpdateLastLoginTime(string cardNumber, DateTime datetime)
-    {
-        _cardRepository.UpdateLastLoginTime(cardNumber, datetime);
-    }
 
     public void UpdateDailyTransferAmount(string cardNumber, float amount)
     {
         _cardRepository.UpdateDailyTransferAmount(cardNumber, amount);
     }
 
-    public int GetCardLoginAttempts(string cardNumber)
-    {
-        return _cardRepository.GetCardLoginAttempt(cardNumber);
-    }
-
-    public DateTime? GetLastLoginTime(string cardNumber)
-    {
-        return _cardRepository.GetLastLoginTime(cardNumber);
-    }
-
-    public void UpdateLoginAttempts(string cardNumber, int attempt)
-    {
-        _cardRepository.UpdateLoginAttempts(cardNumber, attempt);
-    }
 
     public void UpdateBalance(string cardNumber, float amount)
     {
